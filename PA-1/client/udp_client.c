09b91fba97ae34a6f3ad7fb18a2f765330e0579d
Dylan Schneider <DylanDLS@gmail.com>
2018-09-05 15:33:22 -0600

    added first files

---
+++ PA-1/client/udp_client.c
@@ -0,0 +1,148 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <errno.h>
+
+#define MAXBUFSIZE 100
+
+void print_menu();
+
+int main (int argc, char * argv[])
+{
+
+	int nbytes;                             // number of bytes send by sendto()
+	int sock;                               //this will be our socket
+	char buffer[MAXBUFSIZE];
+
+	struct sockaddr_in remote;              //"Internet socket address structure"
+    socklen_t remote_size = sizeof(remote);
+    
+	if (argc < 3)
+	{
+        printf("USAGE: ./client <server_ip> <server_port>\n");
+		exit(1);
+	}
+
+	/******************
+	  Here we populate a sockaddr_in struct with
+	  information regarding where we'd like to send our packet 
+	  i.e the Server.
+	 ******************/
+	bzero(&remote,sizeof(remote));               //zero the struct
+	remote.sin_family = AF_INET;                 //address family
+	remote.sin_port = htons(atoi(argv[2]));      //sets port to network byte order
+	remote.sin_addr.s_addr = inet_addr(argv[1]); //sets remote IP address
+
+	//Causes the system to create a generic socket of type UDP (datagram)
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		printf("unable to create socket");
+	}
+
+	/******************
+	  sendto() sends immediately.  
+	  it will report an error if the message fails to leave the computer
+	  however, with UDP, there is no error if the message is lost in the network once it leaves the computer.
+	 ******************/
+    print_menu();
+    for(;;) {
+        char menu_option[MAXBUFSIZE];
+        char received[MAXBUFSIZE];
+        
+        printf("> ");
+        scanf(" %[^\n]", menu_option);
+        
+        if (strcmp("menu", menu_option) == 0)
+        {
+            print_menu();
+        }
+        else if (strcmp("ls", menu_option) == 0)
+        {
+            if (sendto(sock, menu_option, sizeof(menu_option), 0, (struct sockaddr*) &remote, remote_size) == -1){
+                printf("error sending message");
+                exit(1);
+            }
+            printf("sent ls\n");
+            
+            if (recvfrom(sock, received, sizeof(received), 0, (struct sockaddr*) &remote, &remote_size) == -1){
+                printf("error receiving message");
+                exit(1);
+            }
+            
+            continue;
+        }
+        else if (strcmp("exit", menu_option) == 0)
+        {
+            close(sock);
+            printf("EXITING");
+            exit(0);
+        }
+        
+        else if (strstr(menu_option, "get ") != NULL)
+        {
+            char copy[MAXBUFSIZE];
+            strcpy(copy, menu_option);
+            char *cmd = strtok(copy, " ");
+            char *filename = strtok(NULL, " ");
+            printf("MENU_OPTION:%s\n", menu_option);
+            printf("cmd:%s\n", cmd);
+            printf("name:%s\n", filename);
+        }
+        
+        else if (strstr(menu_option, "put ") != NULL)
+        {
+            char copy[MAXBUFSIZE];
+            strcpy(copy, menu_option);
+            char *cmd = strtok(copy, " ");
+            char *filename = strtok(NULL, " ");
+            printf("MENU_OPTION:%s\n", menu_option);
+            printf("cmd:%s\n", cmd);
+            printf("name:%s\n", filename);
+        }
+        else if (strstr(menu_option, "delete ") != NULL)
+        {
+            
+        }
+        else {
+            printf("Invalid command, try again.\n");
+        }
+        
+    }
+    
+    
+    /*
+	char command[] = "apple";	
+	nbytes = **** CALL SENDTO() HERE ****;
+
+	// Blocks till bytes are received
+	struct sockaddr_in from_addr;
+	int addr_length = sizeof(struct sockaddr);
+	bzero(buffer,sizeof(buffer));
+	nbytes = **** CALL RECVFROM() HERE ****;  
+
+	printf("Server says %s\n", buffer);
+*/
+	close(sock);
+    
+
+}
+
+void print_menu(){
+    printf("Welcome to the Basic UDP Client. Valid commands are:\n");
+    printf("menu - show list of client commands\n");
+    printf("get <filename> - get the file from the server\n");
+    printf("put <filename> - put the file on the server\n");
+    printf("delete <filename> \n");
+    printf("ls \n");
+    printf("exit\n\n");
+}
+
